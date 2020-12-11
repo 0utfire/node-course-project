46dca2420f08bd5b2f65a70094458d432060f424
Roman Romanyuk <nataniel.rr@gmail.com>
2020-12-11 19:38:17 +0200

    Delete index.js

--- task5/dataBase/index.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-module.exports.sequelize = new Sequelize('node-migrations',
-    'root',
-    'Nathan7662', {
-        host: 'localhost',
-        dialect: 'mysql'
-    });
